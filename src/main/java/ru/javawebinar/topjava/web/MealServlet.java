60e5b141f1e4fa8b0b114076c5b39e0d4acdec1a
Mihail Sazonov <sazonov91@gmail.com>
2022-11-29 17:43:53 +0300

    Add Meal Servlet

---
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -0,0 +1,23 @@
+package ru.javawebinar.topjava.web;
+
+import org.slf4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+public class MealServlet extends HttpServlet {
+    private static final Logger log = getLogger(MealServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.debug("forward to meals");
+        req.getRequestDispatcher("/meals.jsp").forward(req, resp);
+//        super.doGet(req, resp);
+    }
+}
---
+++ src/main/webapp/meals.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Mihail
+  Date: 29.11.2022
+  Time: 17:40
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Meals</title>
+</head>
+<body>
+<h3><a href="index.html">Home</a></h3>
+<hr>
+<h2>Meals</h2>
+</body>
+</html>
