e6be22135b5407068befdb94458a2cd13e6fd97d
Mihail Sazonov <sazonov91@gmail.com>
2022-12-06 14:18:55 +0300

    Implement InMemoryUserRepository; Meal extends AbstractBaseEntity; Relocate MealTo to package TransferObject

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -3,43 +3,66 @@ package ru.javawebinar.topjava.repository.inmemory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.AbstractNamedEntity;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
 
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @Repository
 public class InMemoryUserRepository implements UserRepository {
+    private final Map<Integer, User> repository = new ConcurrentHashMap<>();
+    private final AtomicInteger counter = new AtomicInteger(0);
     private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepository.class);
+
+    @Override
+    public User save(User user) {
+        if (user.isNew()) {
+            user.setId(counter.incrementAndGet());
+            repository.put(user.getId(), user);
+            log.info("new user. ID: " + user.getId() +
+                    ";Name: " + user.getName() +
+                    ";Email: " + user.getEmail());
+            return user;
+        }
+        log.info("update user {}", user);
+        return repository.computeIfPresent(user.getId(), (id, oldUser) -> user);
+    }
 
     @Override
     public boolean delete(int id) {
         log.info("delete {}", id);
-        return true;
-    }
-
-    @Override
-    public User save(User user) {
-        log.info("save {}", user);
-        return user;
+        return repository.remove(id) != null;
     }
 
     @Override
     public User get(int id) {
         log.info("get {}", id);
-        return null;
+        return repository.get(id);
     }
 
     @Override
-    public List<User> getAll() {
+    public List<User> getAll() { //sort by Name
         log.info("getAll");
-        return Collections.emptyList();
+        return repository.
+                values().stream().
+                sorted(Comparator.comparing(AbstractNamedEntity::getName)).collect(Collectors.toList());
     }
 
     @Override
     public User getByEmail(String email) {
-        log.info("getByEmail {}", email);
-        return null;
+        for (User user : repository.values()) {
+            if (user.getEmail().equals(email)) {
+                log.info("getByEmail {}", email);
+                return user;
+            }
+        }
+        throw new NotFoundException("User with email: " + email + " not found in DB");
     }
 }
