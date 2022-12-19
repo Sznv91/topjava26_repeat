60ca9f81da10dd6b707aa357d8b2b6638792977b
Mihail Sazonov <sazonov91@gmail.com>
2022-12-19 19:39:09 +0300

    make meal entity

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,19 +1,33 @@
 package ru.javawebinar.topjava.model;
 
+import org.hibernate.annotations.Proxy;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-
+@Entity
+@Table(name = "meals")
 public class Meal extends AbstractBaseEntity {
+    @Column(name = "date_time", nullable = false)
+    @NotNull
     private LocalDateTime dateTime;
 
+    @Column(name = "description", nullable = false)
+    @NotNull
     private String description;
 
+    @Column(name = "calories", nullable = false)
     private int calories;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
     private User user;
 
     public Meal() {
--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
@@ -1,14 +1,22 @@
 package ru.javawebinar.topjava.repository.jpa;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
+@Transactional(readOnly = true)
 public class JpaMealRepository implements MealRepository {
+
+    @PersistenceContext
+    private EntityManager em;
 
     @Override
     public Meal save(Meal meal, int userId) {
@@ -16,13 +24,24 @@ public class JpaMealRepository implements MealRepository {
     }
 
     @Override
+    @Transactional
     public boolean delete(int id, int userId) {
-        return false;
+        return em.createQuery("DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:user_id")
+                .setParameter("id", id)
+                .setParameter("user_id", userId)
+                .executeUpdate() != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+//        Meal meal = em.find(Meal.class, id);
+//        Meal meal = em.createQuery("SELECT m FROM Meal m LEFT JOIN FETCH m.user WHERE m.id=:id", Meal.class).setParameter("id", id)/*.setParameter("user_id", userId)*/.getSingleResult();
+        Meal meal = em.createQuery("SELECT m FROM Meal m WHERE m.id=:id AND m.user.id=:user_id", Meal.class).setParameter("id", id).setParameter("user_id", userId).getSingleResult();
+        return meal;
+        /*if (meal.getUser().getId().equals(userId)) {
+            return meal;
+        }*/
+//        return null;
     }
 
     @Override
