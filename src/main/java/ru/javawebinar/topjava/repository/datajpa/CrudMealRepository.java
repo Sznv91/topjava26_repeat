79b837d39f86a9df8a4367f310cbac95849433e1
Mihail Sazonov <sazonov91@gmail.com>
2022-12-24 18:22:40 +0300

    Make Data-Jpa Meal repository with Query

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,7 +1,29 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+//    @Query(name = Meal.ALL_SORTED)
+    List<Meal> getAll(@Param("userId") int userId);
+
+    List<Meal> getBetween(@Param("userId") int userId, @Param("startDateTime") LocalDateTime start, @Param("endDateTime") LocalDateTime end);
+
+    @Transactional
+    @Modifying
+    int delete(@Param("id") int id, @Param("userId") int userId);
+
+    Meal findMealByIdAndUserId(int id, int userId);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Meal m SET m.calories=:calories, m.dateTime=:date_time, m.description=:description, m.user.id=:user_id WHERE m.id=:id AND m.user.id=:user_id")
+    int updateMealByUserId(@Param("calories") int calories, @Param("date_time") LocalDateTime dateTime, @Param("description") String description, @Param("id") int id, @Param("user_id") int userId);
 }
--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -1,7 +1,10 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
 import java.time.LocalDateTime;
@@ -9,35 +12,44 @@ import java.util.List;
 
 @Repository
 public class DataJpaMealRepository implements MealRepository {
+    private final CrudMealRepository crudMealRepository;
+    private final CrudUserRepository crudUserRepository;
 
-    private final CrudMealRepository crudRepository;
-
-    public DataJpaMealRepository(CrudMealRepository crudRepository) {
-        this.crudRepository = crudRepository;
+    public DataJpaMealRepository(CrudMealRepository crudMealRepository, CrudUserRepository crudUserRepository) {
+        this.crudMealRepository = crudMealRepository;
+        this.crudUserRepository = crudUserRepository;
     }
 
     @Override
+    @Transactional(propagation = Propagation.SUPPORTS)
     public Meal save(Meal meal, int userId) {
-        return null;
+        User user = crudUserRepository.getReferenceById(userId);
+        if (meal.isNew()) {
+            meal.setUser(user);
+            return crudMealRepository.save(meal);
+        } else {
+            int result = crudMealRepository.updateMealByUserId(meal.getCalories(), meal.getDateTime(), meal.getDescription(), meal.getId(), userId);
+            return result > 0 ? meal : null;
+        }
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return crudMealRepository.delete(id, userId) != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        return crudMealRepository.findMealByIdAndUserId(id, userId);
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return crudMealRepository.getAll(userId);
     }
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return null;
+        return crudMealRepository.getBetween(userId, startDateTime, endDateTime);
     }
 }
