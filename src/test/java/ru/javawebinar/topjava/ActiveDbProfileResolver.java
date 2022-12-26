f82d52f9a2e7a90d04ff6a3dd7cb01aa405f2f36
Mihail Sazonov <sazonov91@gmail.com>
2022-12-26 22:25:17 +0300

    Make profiles JDBC JPA DATAJPA

--- src/test/java/ru/javawebinar/topjava/ActiveDbProfileResolver.java
+++ src/test/java/ru/javawebinar/topjava/ActiveDbProfileResolver.java
@@ -8,6 +8,6 @@ public class ActiveDbProfileResolver implements ActiveProfilesResolver {
     @Override
     public @NonNull
     String[] resolve(@NonNull Class<?> aClass) {
-        return new String[]{Profiles.getActiveDbProfile()};
+        return new String[]{Profiles.getActiveDbProfile(), Profiles.REPOSITORY_IMPLEMENTATION};
     }
 }
