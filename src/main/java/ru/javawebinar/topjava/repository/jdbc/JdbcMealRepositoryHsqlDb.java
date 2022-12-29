3c5bcdb223961e2bbbdcadc5a4c7bcf48bcd97f1
Mihail Sazonov <sazonov91@gmail.com>
2022-12-30 00:01:51 +0300

    Optional 6 - Split JdbcMealRepository for HSQLDB and Postgres with pattern TemplateMethod and Spring profiles

---
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryHsqlDb.java
@@ -0,0 +1,40 @@
+package ru.javawebinar.topjava.repository.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.Meal;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+@Profile("hsqldb")
+public class JdbcMealRepositoryHsqlDb extends JdbcMealRepository {
+
+    @Autowired
+    public JdbcMealRepositoryHsqlDb(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        super(jdbcTemplate, namedParameterJdbcTemplate);
+    }
+
+    @Override
+    protected MapSqlParameterSource getMap(Meal meal, int userId) {
+        return new MapSqlParameterSource()
+                .addValue("id", meal.getId())
+                .addValue("description", meal.getDescription())
+                .addValue("calories", meal.getCalories())
+                .addValue("date_time", Timestamp.valueOf(meal.getDateTime()))
+                .addValue("user_id", userId);
+    }
+
+    @Override
+    public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
+        return jdbcTemplate.query(
+                "SELECT * FROM meals WHERE user_id=?  AND date_time >=  ? AND date_time < ? ORDER BY date_time DESC",
+                ROW_MAPPER, userId, Timestamp.valueOf(startDateTime), Timestamp.valueOf(endDateTime));
+    }
+}
