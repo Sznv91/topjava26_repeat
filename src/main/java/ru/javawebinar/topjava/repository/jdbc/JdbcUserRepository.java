ff886bc221317424f4e682ebd34eecbfb5ff22f7
Mihail Sazonov <sazonov91@gmail.com>
2023-01-05 11:27:04 +0300

    Add roles into user in get operation in JDBC repository

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
@@ -4,16 +4,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Repository
+@Transactional(readOnly = true)
 public class JdbcUserRepository implements UserRepository {
 
     private static final BeanPropertyRowMapper<User> ROW_MAPPER = BeanPropertyRowMapper.newInstance(User.class);
@@ -35,6 +45,7 @@ public class JdbcUserRepository implements UserRepository {
     }
 
     @Override
+    @Transactional
     public User save(User user) {
         BeanPropertySqlParameterSource parameterSource = new BeanPropertySqlParameterSource(user);
 
@@ -51,6 +62,7 @@ public class JdbcUserRepository implements UserRepository {
     }
 
     @Override
+    @Transactional
     public boolean delete(int id) {
         return jdbcTemplate.update("DELETE FROM users WHERE id=?", id) != 0;
     }
@@ -70,6 +82,58 @@ public class JdbcUserRepository implements UserRepository {
 
     @Override
     public List<User> getAll() {
-        return jdbcTemplate.query("SELECT * FROM users ORDER BY name, email", ROW_MAPPER);
+
+        class RoleToRowMapper implements RowMapper<RoleToRowMapper.RoleTo> {
+            class RoleTo {
+                int userId;
+                Role role;
+
+                public int getUserId() {
+                    return userId;
+                }
+
+                public void setUserId(int userId) {
+                    this.userId = userId;
+                }
+
+                public Role getRole() {
+                    return role;
+                }
+
+                public void setRole(Role role) {
+                    this.role = role;
+                }
+            }
+
+            @Override
+            public RoleTo mapRow(ResultSet rs, int rowNum) throws SQLException {
+                RoleTo roleTo = new RoleTo();
+                int userId = rs.getInt("user_id");
+                roleTo.setUserId(userId);
+                String role = rs.getString("role");
+                if (role != null) {
+                    roleTo.setRole(Role.valueOf(role));
+                }
+                return roleTo;
+            }
+        }
+
+        List<User> userList = jdbcTemplate.query("SELECT * FROM users ORDER BY name, email", ROW_MAPPER);
+        List<RoleToRowMapper.RoleTo> roleToList = jdbcTemplate.query("SELECT * FROM user_roles", new RoleToRowMapper());
+        Map<Integer, List<Role>> roleMap = new HashMap<>();
+        for (RoleToRowMapper.RoleTo roleTo : roleToList) {
+            if (roleTo.role != null) {
+                roleMap.merge(roleTo.getUserId(), Collections.singletonList(roleTo.getRole()), (oldVal, newVal) -> {
+                    List<Role> role = new ArrayList<>();
+                    role.addAll(oldVal);
+                    role.addAll(newVal);
+                    return role;
+                });
+            }
+        }
+        userList.forEach(user -> user.setRoles(roleMap.getOrDefault(user.getId(), new ArrayList<>())));
+        return userList;
     }
+
+
 }
