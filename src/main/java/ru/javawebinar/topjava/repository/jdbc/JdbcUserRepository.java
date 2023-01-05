44a52a05373ed99a8054d881a2250881474da7fb
Mihail Sazonov <sazonov91@gmail.com>
2023-01-05 15:04:01 +0300

    Add save&update roles in JdbcUserRepository, and fix tests

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
@@ -2,6 +2,7 @@ package ru.javawebinar.topjava.repository.jdbc;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -14,8 +15,10 @@ import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -33,12 +36,16 @@ public class JdbcUserRepository implements UserRepository {
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     private final SimpleJdbcInsert insertUser;
+    private final SimpleJdbcInsert insertRole;
 
     @Autowired
     public JdbcUserRepository(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
         this.insertUser = new SimpleJdbcInsert(jdbcTemplate)
                 .withTableName("users")
                 .usingGeneratedKeyColumns("id");
+
+        this.insertRole = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("user_roles");
 
         this.jdbcTemplate = jdbcTemplate;
         this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
@@ -52,11 +59,59 @@ public class JdbcUserRepository implements UserRepository {
         if (user.isNew()) {
             Number newKey = insertUser.executeAndReturnKey(parameterSource);
             user.setId(newKey.intValue());
-        } else if (namedParameterJdbcTemplate.update("""
-                   UPDATE users SET name=:name, email=:email, password=:password, 
-                   registered=:registered, enabled=:enabled, calories_per_day=:caloriesPerDay WHERE id=:id
-                """, parameterSource) == 0) {
-            return null;
+
+            List<Role> roleList = new ArrayList<>(user.getRoles());
+            for (Role role : roleList) {
+                Map<String, Object> param = new HashMap<>();
+                param.put("user_id", user.getId());
+                param.put("role", role);
+                insertRole.execute(param);
+            }
+        } else {
+            int result = 0;
+            int[] userUpdateBatchResult = jdbcTemplate.batchUpdate(
+                    "UPDATE users SET name=?, email=?, password=?, registered=?, enabled=?, calories_per_day=? WHERE id=?",
+                    new BatchPreparedStatementSetter() {
+                        @Override
+                        public void setValues(PreparedStatement ps, int i) throws SQLException {
+                            ps.setString(1, user.getName());
+                            ps.setString(2, user.getEmail());
+                            ps.setString(3, user.getPassword());
+                            ps.setTimestamp(4, new Timestamp(user.getRegistered().getTime()));
+                            ps.setBoolean(5, user.isEnabled());
+                            ps.setInt(6, user.getCaloriesPerDay());
+                            ps.setInt(7, user.getId());
+                        }
+
+                        @Override
+                        public int getBatchSize() {
+                            return 1;
+                        }
+                    });
+            int[] roleUpdateBatchResult = jdbcTemplate.batchUpdate("UPDATE user_roles SET role=? WHERE user_id=?",
+                    new BatchPreparedStatementSetter() {
+                        @Override
+                        public void setValues(PreparedStatement ps, int i) throws SQLException {
+                            List<Role> roleList = new ArrayList<>(user.getRoles());
+                            ps.setString(1, roleList.get(i).toString());
+                            ps.setInt(2, user.getId());
+                        }
+
+                        @Override
+                        public int getBatchSize() {
+                            return user.getRoles().size();
+                        }
+                    });
+
+            for (int i : userUpdateBatchResult) {
+                result += i;
+            }
+            for (int i : roleUpdateBatchResult) {
+                result += i;
+            }
+            if (result == 0) {
+                return null;
+            } else return user;
         }
         return user;
     }
@@ -70,56 +125,30 @@ public class JdbcUserRepository implements UserRepository {
     @Override
     public User get(int id) {
         List<User> users = jdbcTemplate.query("SELECT * FROM users WHERE id=?", ROW_MAPPER, id);
-        return DataAccessUtils.singleResult(users);
+        List<RoleToRowMapper.RoleTo> roleTos = jdbcTemplate.query("SELECT * FROM user_roles WHERE user_id=?", new RoleToRowMapper(), id);
+        return DataAccessUtils.singleResult(mergeRole(users, roleTos));
     }
 
     @Override
     public User getByEmail(String email) {
 //        return jdbcTemplate.queryForObject("SELECT * FROM users WHERE email=?", ROW_MAPPER, email);
         List<User> users = jdbcTemplate.query("SELECT * FROM users WHERE email=?", ROW_MAPPER, email);
-        return DataAccessUtils.singleResult(users);
+        if (!users.isEmpty()) {
+            List<RoleToRowMapper.RoleTo> roleTos = jdbcTemplate.query("SELECT * FROM user_roles WHERE user_id=?", new RoleToRowMapper(), users.get(0).getId());
+            return DataAccessUtils.singleResult(mergeRole(users, roleTos));
+        }
+        return null;
     }
 
     @Override
     public List<User> getAll() {
 
-        class RoleToRowMapper implements RowMapper<RoleToRowMapper.RoleTo> {
-            class RoleTo {
-                int userId;
-                Role role;
-
-                public int getUserId() {
-                    return userId;
-                }
-
-                public void setUserId(int userId) {
-                    this.userId = userId;
-                }
-
-                public Role getRole() {
-                    return role;
-                }
-
-                public void setRole(Role role) {
-                    this.role = role;
-                }
-            }
-
-            @Override
-            public RoleTo mapRow(ResultSet rs, int rowNum) throws SQLException {
-                RoleTo roleTo = new RoleTo();
-                int userId = rs.getInt("user_id");
-                roleTo.setUserId(userId);
-                String role = rs.getString("role");
-                if (role != null) {
-                    roleTo.setRole(Role.valueOf(role));
-                }
-                return roleTo;
-            }
-        }
-
         List<User> userList = jdbcTemplate.query("SELECT * FROM users ORDER BY name, email", ROW_MAPPER);
         List<RoleToRowMapper.RoleTo> roleToList = jdbcTemplate.query("SELECT * FROM user_roles", new RoleToRowMapper());
+        return mergeRole(userList, roleToList);
+    }
+
+    private List<User> mergeRole(List<User> userList, List<RoleToRowMapper.RoleTo> roleToList) {
         Map<Integer, List<Role>> roleMap = new HashMap<>();
         for (RoleToRowMapper.RoleTo roleTo : roleToList) {
             if (roleTo.role != null) {
@@ -132,8 +161,42 @@ public class JdbcUserRepository implements UserRepository {
             }
         }
         userList.forEach(user -> user.setRoles(roleMap.getOrDefault(user.getId(), new ArrayList<>())));
-        return userList;
+        return new ArrayList<>(userList);
     }
 
+    private class RoleToRowMapper implements RowMapper<RoleToRowMapper.RoleTo> {
+        class RoleTo {
+            int userId;
+            Role role;
+
+            public int getUserId() {
+                return userId;
+            }
+
+            public void setUserId(int userId) {
+                this.userId = userId;
+            }
+
+            public Role getRole() {
+                return role;
+            }
+
+            public void setRole(Role role) {
+                this.role = role;
+            }
+        }
+
+        @Override
+        public RoleTo mapRow(ResultSet rs, int rowNum) throws SQLException {
+            RoleTo roleTo = new RoleTo();
+            int userId = rs.getInt("user_id");
+            roleTo.setUserId(userId);
+            String role = rs.getString("role");
+            if (role != null) {
+                roleTo.setRole(Role.valueOf(role));
+            }
+            return roleTo;
+        }
+    }
 
 }
--- src/test/java/ru/javawebinar/topjava/UserTestData.java
+++ src/test/java/ru/javawebinar/topjava/UserTestData.java
@@ -3,13 +3,14 @@ package ru.javawebinar.topjava;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
 
 import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
 
 public class UserTestData {
-    public static final MatcherFactory.Matcher<User> USER_MATCHER = MatcherFactory.usingIgnoringFieldsComparator("registered", "roles", "meals");
+    public static final MatcherFactory.Matcher<User> USER_MATCHER = MatcherFactory.usingIgnoringFieldsComparator("registered", "meals");
 
     public static final int USER_ID = START_SEQ;
     public static final int ADMIN_ID = START_SEQ + 1;
@@ -17,11 +18,11 @@ public class UserTestData {
     public static final int NOT_FOUND = 10;
 
     public static final User user = new User(USER_ID, "User", "user@yandex.ru", "password", Role.USER);
-    public static final User admin = new User(ADMIN_ID, "Admin", "admin@gmail.com", "admin", Role.ADMIN);
+    public static final User admin = new User(ADMIN_ID, "Admin", "admin@gmail.com", "admin", Role.USER, Role.ADMIN);
     public static final User guest = new User(GUEST_ID, "Guest", "guest@gmail.com", "guest");
 
     public static User getNew() {
-        return new User(null, "New", "new@gmail.com", "newPass", 1555, false, new Date(), Collections.singleton(Role.USER));
+        return new User(null, "New", "new@gmail.com", "newPass", 1555, false, new Date(), Arrays.asList(Role.USER, Role.ADMIN));
     }
 
     public static User getUpdated() {
